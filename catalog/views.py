d441e087326d217942b26c6dcdf29f0bcc5936f3
PJ-BN <63279581+PJ-BN@users.noreply.github.com>
2023-08-05 19:25:29 +0545

    added content to profile

--- catalog/views.py
+++ catalog/views.py
@@ -17,11 +17,30 @@ def home(request):
 
 def profile(request):
     if request.method =='GET':
-        return render(request, 'profile.html')
+        context = {
+        "data" : [1,2,3,4,5,6,7,8,9,10],
+        "f_name": "Prajwal",
+        "l_name":"Bhandari",
+        "email" : "prajwalbh25@gmail.com",
+        "address":"Banbatika",
+        "phone" : "9841615431",
+        "order": ["1234"], 
+        "time":["01/01/2000"],
+        "items": ["a.png"],
+        "total": ["2000"]
+        
+            }
+        return render(request, 'profile.html', context)
     
 def cart(request):
     if request.method == "GET":
         context = {
-        "data" : [1,2,3,4,5,6,7,8,9,10]
+        "data" : [1,2,3,4,5,6,7,8,9,10],
+        "f_name": "Prajwal",
+        "l_name":"Bhandari",
+        "email" : "prajwalbh25@gmail.com",
+        "address":"Banbatika",
+        "phone" : "9841615431" 
+        
             }
         return render(request, 'cart.html', context)
