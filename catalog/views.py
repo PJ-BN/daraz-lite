40c0d8a5086ae272721b3ab1200efd24eda6e492
PJ-BN <63279581+PJ-BN@users.noreply.github.com>
2023-08-07 14:19:30 +0545

    working smooth navigation

--- catalog/views.py
+++ catalog/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
-def landing(request):
-    context = {
-        "data" : [1,2,3,4,5,6,7,8,9,10]
-    }
-    return render(request, 'landing.html', context)
+def Username(request):
+    if 'username' in request.session:
+        username=request.session['username']
+    else:
+        username = "" 
+    
+    return username
+
 
 def logout(request):
     del request.session["username"]
@@ -15,9 +18,9 @@ def logout(request):
 
 
 def home(request):
-    username = ""
-    if 'username' in request.session:
-        username=request.session['username']
+    username = Username(request)
+    
+    
         
     context = {
         "data" : [1,2,3,4,5,6,7,8,9,10],
@@ -30,6 +33,8 @@ def home(request):
 
 def profile(request):
     if request.method =='GET':
+        username = Username(request)
+        
         context = {
         "data" : [1,2,3,4,5,6,7,8,9,10],
         "f_name": "Prajwal",
@@ -40,20 +45,25 @@ def profile(request):
         "order": ["1234"], 
         "time":["01/01/2000"],
         "items": ["a.png"],
-        "total": ["2000"]
+        "total": ["2000"],
+        "username": username
         
             }
         return render(request, 'profile.html', context)
     
 def cart(request):
     if request.method == "GET":
+        username = Username(request)
+        
         context = {
         "data" : [1,2,3,4,5,6,7,8,9,10],
         "f_name": "Prajwal",
         "l_name":"Bhandari",
         "email" : "prajwalbh25@gmail.com",
         "address":"Banbatika",
-        "phone" : "9841615431" 
+        "phone" : "9841615431" ,
+        "username": username
+        
         
             }
         return render(request, 'cart.html', context)
