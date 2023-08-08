a0adf4f45d95426e35dd4cd791855cc930fa553e
PJ-BN <63279581+PJ-BN@users.noreply.github.com>
2023-08-08 12:18:45 +0545

    user authentication

--- login/views.py
+++ login/views.py
@@ -1,29 +1,30 @@
 from django.shortcuts import render , redirect
 from .models import *
 from catalog.views import home
+from django.http import HttpResponse 
+from django.contrib.auth.models import User
 
 # Create your views here.
-from django.http import HttpResponse 
 
 
 def page(request):
     return render(request,'login/index.html')
     
-def login(request):
-    if request.method == "POST":
-        username = request.POST.get('email')
-        password = request.POST.get('pswd')
-        quer = Loginid.objects.all().values()
-        for i in quer:
-            if i["email"] == username:
-                if i["password"] == password:
-                    request.session['username']= username
-                    request.session.save()
-                    return redirect(home)
+# def login(request):
+#     if request.method == "POST":
+#         username = request.POST.get('email')
+#         password = request.POST.get('pswd')
+#         quer = Loginid.objects.all().values()
+#         for i in quer:
+#             if i["email"] == username:
+#                 if i["password"] == password:
+#                     request.session['username']= username
+#                     request.session.save()
+#                     return redirect(home)
                 
-        return HttpResponse("Incorrect")
-    elif request.method =="GET":
-        return redirect(page)
+#         return HttpResponse("Incorrect")
+#     elif request.method =="GET":
+#         return redirect(page)
             
             
         
@@ -34,17 +35,21 @@ def signup(request):
     if request.method == "POST":
         username = request.POST.get('email')
         password = request.POST.get('pswd')
-        name = request.POST.get("name")
-        quer = Loginid.objects.values_list("email")
-        quer = [i[0] for i in quer]
-        id = len(quer) + 1 
-        print(quer)
-        if username not in quer:
-            l = Loginid()
-            l.name = name
-            l.email = username
-            l.password = password
-            l.id = id
-            l.save()
-            return redirect("page")
-        return HttpResponse("this username already exisis")
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        
+        # quer = Loginid.objects.values_list("email")
+        # quer = [i[0] for i in quer]
+        # id = len(quer) + 1 
+        # print(quer)
+        # if username not in quer:
+        #     l = Loginid()
+        #     l.name = name
+        #     l.email = username
+        #     l.password = password
+        #     l.id = id
+        #     l.save()
+        #     return redirect("page")
+        # return HttpResponse("this username already exisis")
+        
+        user = User.objects.create_user(username=username, password=password , first_name = first_name, last_name = last_name )
