fd5c00493acbad72f6f4f0f5423aaacb042d85cc
PJ-BN <63279581+PJ-BN@users.noreply.github.com>
2023-11-30 15:25:10 +0545

    udated index.js

--- catalog/static/js/index.js
+++ catalog/static/js/index.js
@@ -1,18 +1,26 @@
 function cart() {
-    var form_data = "helloworld";
-    console.log(form_data)
-    $.ajax({
-        url: "{% url 'cart' %}",
-        type: "POST",
-        data: form_data,
-        cache: false
-    }).done(function(data) {
-        if (data.result === true) {
-            alert(data.message);
-
-        }
-    });
+    fetch('{% url "home" %}', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': '{{ csrf_token }}' // Include CSRF token for security
+            },
+            body: JSON.stringify({
+                // Add any data you want to send in the request body
+                key1: 'value1',
+                key2: 'value2',
+                // ...
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            // Handle the response as needed
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 
 }
 
-document.getElementById("buy_now").addEventListener("click", cart)
+// document.getElementById("buy_now").addEventListener("click", cart)
