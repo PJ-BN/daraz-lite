3cce0121d5afc49c68a5b591d6fc9f25a32c8b60
PJ-BN <63279581+PJ-BN@users.noreply.github.com>
2023-12-13 16:41:21 +0545

    cart ui change

--- catalog/static/js/cart.js
+++ catalog/static/js/cart.js
@@ -26,24 +26,45 @@ document.getElementById("quantity_subt").addEventListener("click", function() {
 })
 
 console.log(page_checkbox)
-console.log(product_checkbox)
+    // console.log(product_checkbox)
 
 
 let ab = []
 let cd = []
+let product = []
 
-for (let i = 0; i < page_checkbox.length; i++) {
+// for (let i = 0; i < page_checkbox.legyh; i++) {
+//     let a = []
+//     a.push("product_checkbox{{i.id.id}}")
+//     console.log(a)
+//     product.push(a)
 
-    ab.push(document.getElementById(page_checkbox[i]))
-    ab[i].addEventListener('change', function() {
-        cd.push(document.getElementById(product_checkbox[i]))
-        if (ab[i].checked) {
-            console.log("done");
-            cd[i].checked = true
+// }
+console.log(page)
 
-        } else {
-            cd[i].checked = false
+for (let key in page) {
+    ab.push(document.getElementById("page_checkbox_" + key))
+    for (let value of page[key]) {
+        cd.push(document.getElementById("product_checkbox_" + value))
 
-        }
-    })
+
+    }
 }
+
+console.log(ab, cd)
+
+// for (let i = 0; i < page_checkbox.length; i++) {
+
+//     ab.push(document.getElementById(page_checkbox[i]))
+//     ab[i].addEventListener('change', function() {
+//         cd.push(document.getElementById(product_checkbox[i]))
+//         if (ab[i].checked) {
+//             console.log("done");
+//             cd[i].checked = true
+
+//         } else {
+//             cd[i].checked = false
+
+//         }
+//     })
+// }
