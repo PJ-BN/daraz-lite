16490c679903b16d0977c3893692239a4d2b6282
PJ-BN <63279581+PJ-BN@users.noreply.github.com>
2023-12-18 19:54:29 +0545

    change in login page

--- login/views.py
+++ login/views.py
@@ -8,45 +8,38 @@ from django.contrib.auth import login as auth_login
 
 from django.contrib import messages
 
-# Create your views here.
-
-
-# def page(request):
-#     return render(request,'login/index.html')
-    
+  
 def login(request):
     if request.method == "POST":
         username = request.POST.get('email')
         password = request.POST.get('pswd')
         user = authenticate(request, username= username, password=password)
-        auth_login( request, user)
+        try:
+            auth_login( request, user)
+            
+        except:
+            return render(request, "login\index.html", {"data": "Incorect username and password"})
+            
         return redirect(home)
     elif request.method =="GET":
         return render(request, "login\index.html")
-            
-            
-        
-        
-        
+     
     
 def signup(request):
     if request.method == "POST":
-        username = request.POST.get('email')
+        username = request.POST.get('username')
         password = request.POST.get('pswd')
         first_name = request.POST.get("first_name")
         last_name = request.POST.get("last_name")
         address = request.POST.get("address")
-        name = first_name+ " " + last_name
-        datab = Loginid(email = username, address = address, name= name, password = password )
+        email  = request.POST.get("email")
         
-        
-        user = User.objects.create_user(username=username, password=password , first_name = first_name, last_name = last_name )
+        user = User.objects.create_user(username=username, password=password , first_name = first_name, last_name = last_name, email= email )
         
         if user is not None:
             user.save()
             us = authenticate(request, username= username, password=password)
             auth_login(request, us)
-            datab.save()
         
             return redirect(home)
         
